150486d5ff12bd4eced6b42550bf546344438dd8
Dexter Valencia <deeside@pacbell.net>
2020-02-09 23:01:16 -0800

    pictures are too big, needs css

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -5,25 +5,65 @@ import Score from "./components/Score";
 import friends from "./friends.json";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+  state = {
+    friends,
+    clickedFriendsIds: [],
+    score: 0,
+    goal: 8,
+    status: ""
+  };
+
+  shuffleScoreCard = id => {
+    let clickedFriendsIds = this.state.clickedFriendsIds;
+
+    if(clickedFriendsIds.includes(id)){
+      this.setState({ clickedFriendsIds: [], score: 0, status:  "You Guessed incorrectly!" });
+      return;
+    }else{
+      clickedFriendsIds.push(id)
+
+      if(clickedFriendsIds.length === 8){
+        this.setState({score: 8, status: "You guessed correctly", clickedFriendsIds: []});
+        console.log('You Win');
+        return;
+      }
+
+      this.setState({ friends, clickedFriendsIds, score: clickedFriendsIds.length, status: " " });
+
+      for (let i = friends.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [friends[i], friends[j]] = [friends[j], friends[i]];
+      }
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Clicky Game</h1>
+          <p className="App-intro">
+          Click on an image to earn points, but don't click on any more than once!
+          </p>
+        </header>
+        <Score total={this.state.score}
+        goal={8}
+        status={this.state.status}
+        />
+    <Wrapper>
+            {this.state.friends.map(friend => (
+    <Card
+        shuffleScoreCard={this.shuffleScoreCard}
+        id={friend.id}
+        key={friend.id}
+        image={friend.image}
+        />
+          ))}
+    </Wrapper>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
