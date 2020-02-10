87cc33f26d6086dcac90e8f25e4246618e30cad5
Dexter Valencia <deeside@pacbell.net>
2020-02-09 22:27:12 -0800

    File structure for Card, Score, Wrapper

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,6 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Card from "./components/Card";
+import Wrapper from "./components/Wrapper";
+import Score from "./components/Score";
+import friends from "./friends.json";
+import "./App.css";
 
 function App() {
   return (
